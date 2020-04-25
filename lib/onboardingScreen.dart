522ab8b972ae03a0efc461f883084a7743d6a1a5
musumbidenis <musumbidenis@gmail.com>
2020-04-25 14:52:43 +0300

    Added indicator

--- lib/onboardingScreen.dart
+++ lib/onboardingScreen.dart
@@ -7,9 +7,30 @@ class OnboardingScreen extends StatefulWidget {
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
 
-  final int _pages = 3;
+  final int _pages = 2;
   int _currentPage = 0;
   final PageController _pageController = PageController(initialPage: 0);
+
+  List<Widget> _buildIndicator(){
+    List<Widget> list = [];
+
+    for(int i = 0; i <_pages; i++){
+    list.add(i == _currentPage ? _indicator(true): _indicator(false));
+    }
+    return list;
+  }
+
+  Widget _indicator(bool isActive){
+    return AnimatedContainer(duration:Duration(milliseconds: 150),
+    margin: EdgeInsets.symmetric(horizontal:8.0),
+    height: 8.0,
+    width: isActive ? 24.0 : 16.0,
+    decoration: BoxDecoration(
+      color: isActive ? Colors.white : Color(0xFF7B51D3),
+    )
+    );
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,11 +105,53 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                         ],
                       ),
                     ),
+
+                    //Asset image
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:50),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          
+                          Image(
+                            image: AssetImage('assets/01.png'),
+                            height: 300,
+                            width: 300,
+                          ),
+
+                          SizedBox(height: 20,),
+                          
+                          //Title
+                            Text(
+                              "Testing Onboarding Screen",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          
+                          SizedBox(height: 20),
+
+                          //Subtitle
+                          Text(
+                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra tellus nulla, tristique euismod augue. ",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,),
+                            ),
+                          
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
 
-              
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _buildIndicator(),
+            ),
               
             ],
           ),
